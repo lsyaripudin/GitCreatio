c50c2b5bbd3ce9a024876596aee0dc3cff37de18
Lili Syaripudin <lsyaripudin@gmail.com>
2025-11-05 15:12:45 +0700

    Push Sekolah

---
+++ UsrSekolah/Schemas/UsrSiswa_Detail/UsrSiswa_Detail.js
@@ -0,0 +1,8 @@
+define('UsrSiswa_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrSiswa',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
