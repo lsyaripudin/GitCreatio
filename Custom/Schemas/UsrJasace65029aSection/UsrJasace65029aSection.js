c63e315c52b014d1392fc263ff5ca7b3c8f5c0d5
Lili Syaripudin <lsyaripudin@gmail.com>
2025-11-07 13:56:21 +0700

    Update Jasa

---
+++ Custom/Schemas/UsrJasace65029aSection/UsrJasace65029aSection.js
@@ -0,0 +1,8 @@
+define("UsrJasace65029aSection", [], function() {
+	return {
+		entitySchemaName: "UsrJasa",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
